629eb23c812713619fa7a01b0750bb4c981b1c3c
leslierere <35732879+leslierere@users.noreply.github.com>
2018-11-06 11:12:31 +0800

    Update simpleExp.py

--- simpleExp.py
+++ simpleExp.py
@@ -3,7 +3,7 @@ from threading import Thread, Timer
 
 
 def timedTask():  # the task you want to run for every specific period
-    print('you will achieve it as long as you believe it')
+    print('you will achieve it as long as you believe it')#where you put the codes what you really need to run
     t = Timer(10, timedTask)
     #after 10 seconds, timedTask will be started
     #timedTask will be run in a separate thread of control
@@ -13,3 +13,8 @@ def timedTask():  # the task you want to run for every specific period
 
 if __name__ == '__main__':
     timedTask()
+
+#Notes
+#this is applicable to pretty easy task
+#"The interval the timer will wait before executing its action may not be exactly the same as the interval specified by the user."
+#ref: https://docs.python.org/3/library/threading.html
