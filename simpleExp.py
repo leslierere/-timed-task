0ab3a6ae888c5dca53ef5958d0964df69790ab31
leslierere <35732879+leslierere@users.noreply.github.com>
2018-11-06 14:14:56 +0800

    Update simpleExp.py
    change to my own method of timed task

--- simpleExp.py
+++ simpleExp.py
@@ -3,18 +3,17 @@ from threading import Thread, Timer
 
 
 def timedTask():  # the task you want to run for every specific period
-    print('you will achieve it as long as you believe it')#where you put the codes what you really need to run
-    t = Timer(10, timedTask)
-    #after 10 seconds, timedTask will be started
-    #timedTask will be run in a separate thread of control
-    t.start()# start the seperate thread of control, and the thread will stop being "alive" when the method terminates
-
-
+    print('you will achieve it as long as you believe it')
+   
 
 if __name__ == '__main__':
-    timedTask()
+    while True:
+    t = Thread(target= timedTask())#timedTask will be run in a separate thread of control
+    t.start()# start the seperate thread of control, and the thread will stop being "alive" when the method terminates
+    var = (random.random()*10)
+    print(t.is_alive())# return bool, to know whether this thread is down
+    print("接下来睡",var, "秒")
+    time.sleep(var)
 
-#Notes
-#this is applicable to pretty easy task
-#"The interval the timer will wait before executing its action may not be exactly the same as the interval specified by the user."
+
 #ref: https://docs.python.org/3/library/threading.html
